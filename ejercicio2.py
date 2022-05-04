d8e337c05728c306c0efd83b66082a4a52795f68
Davidgraja <juliograjalesparedes127@gmail.com>
2022-05-04 23:51:54 +0200

    ejercicios de la unidad 1 de  programacion en python Mintic

---
+++ ejercicio2.py
@@ -0,0 +1,33 @@
+# print("      *")
+# print("      * *")
+# print("*******   *")
+# print("*           *")
+# print("*******   *")
+# print("      * *")
+# print("      *")
+
+
+# print("""
+#              *
+#              * *
+#       *******    *
+#       *            *
+#       *******    *
+#              * *
+#              *
+# """)
+
+# print(""" Nombre    : Honorio Fernadón Arcinietas
+#  Dirección : Barrio Las Camelias Mz = 8 # 16
+#  Telefono : 32156585
+#  Email : honoriofer@mail.com
+#  Edad : 24 años
+# """)
+
+
+print("""Nombre del cliente       : carlos Roncancio
+Número de identificacion : 89003657
+""")
+print("_","_","_","_","_" ,sep="_________________")
+print("Producto" ,end="        |")
+print("cantidad")
