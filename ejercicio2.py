3235c4e800ded553be96824f4573ecc7c66046c8
Davidgraja <juliograjalesparedes127@gmail.com>
2022-05-05 21:43:33 +0200

    ejercicio N# 4 terminado

--- ejercicio2.py
+++ ejercicio2.py
@@ -25,9 +25,29 @@
 # """)
 
 
-print("""Nombre del cliente       : carlos Roncancio
-Número de identificacion : 89003657
-""")
-print("_","_","_","_","_" ,sep="_________________")
-print("Producto" ,end="        |")
-print("cantidad")
+
+
+# print("Nombre del cliente       : carlos Roncancio")
+# print("Número de identificacion : 89003657")
+# print("-","-","-","-","-" ,sep="--------------------" )
+# print("Producto","Cantidad","Valor Unitario","Valor Total",sep="          |")
+# print("-","-","-","-","-" ,sep="--------------------" )
+# print("Arroz Blanco","3",sep="      |", end="                 ")
+# print("","$1500",sep="|", end="                   ")
+# print("","$4500",sep="|")
+#
+# print("Mantequilla","1",sep="       |", end="                 ")
+# print("","$3200",sep="|", end="                   ")
+# print("","$3200",sep="|")
+#
+# print("Queso Crema","1",sep="       |", end="                 ")
+# print("","$2300",sep="|", end="                   ")
+# print("","$4600",sep="|")
+#
+# print("-","-","-","-","-" ,sep="--------------------" )
+# print("","","Total",sep="                  |",end="                   ")
+# print("","$12300", sep="|")
+# print("-","-","-","-","-" ,sep="--------------------" )
+# print("Forma de pago :")
+# print("#","Efectivo",sep="   ",end="  #  ")
+# print("Tarjeta Débito","Targeta Crédito","Puntos de Nuestro Almacen",sep="   #   ")
--- reto1.py
+++ reto1.py
@@ -1,17 +1,15 @@
 def CDT(usuario , capital , tiempo):
     if tiempo > 2:
-
-        #calculo de los intereses ganados:
         porcentajeIntereses = 0.03
         valorIntereses = (capital * porcentajeIntereses * tiempo) / 12
         valorTotal = valorIntereses + capital
-        print(f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {int(valorTotal)}")
+        return f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valorTotal}"
 
     elif tiempo <= 2:
-        #calculo de la perdida de interses:
         porcentaje_de_perdida = 0.02
         valor_A_perder = capital * porcentaje_de_perdida
         valorTotal = capital - valor_A_perder
-        print(f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {int(valorTotal)}")
+        return f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valorTotal}"
 
-calculoCdt("AB1012",1000000,3)
+
+print(CDT("AB1012 ",100000,3))
