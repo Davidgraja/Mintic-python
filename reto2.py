6c5ffa1b4a2f75dc7f17c5e76e33a7e61230a02a
Davidgraja <juliograjalesparedes127@gmail.com>
2022-05-14 21:21:42 +0200

    nueva version del rreto dos , se tenia que implementar de otra manera

--- reto2.py
+++ reto2.py
@@ -1,78 +1,80 @@
-def cliente(id_cliente,nombre,edad,primer_ingreso):
-    informacion_cliente= dict(id_cliente= id_cliente, nombre= nombre,edad=edad,primer_ingreso=primer_ingreso)
-      
-    descuentoXtreme_sillas = 0.05
-    descuentoCarrochocones = 0.07    
-    informacion_final = dict(nombre = informacion_cliente["nombre"],edad=informacion_cliente["edad"],atraccion="",apto= "", primer_ingreso=informacion_cliente["primer_ingreso"], total_boleta="" )
-    
-    """validacion"""
+informacion = {
+    "id_cliente" : 4,
+    "nombre" : "Tatiana Ruiz",
+    "edad" : 8,
+    "primer_ingreso"  : False
+}
 
-    #> de 18 años:
-    if informacion_cliente.get("edad") > 18 and informacion_cliente.get("primer_ingreso") ==True : 
+
+def cliente(informacion):
+    
+    descuentoXtreme_sillas = 0.05
+    descuentoCarrochocones = 0.07
+    descuentoCarrochocones = 0.07  
+    
+    informacion_final = dict(nombre=informacion["nombre"],edad=informacion["edad"], atraccion = "", apto="", primer_ingreso=informacion["primer_ingreso"], total_boleta="")
+    
+    if informacion.get("edad") > 18 and informacion.get("primer_ingreso") ==True : 
         atraccion = "X-Treme"
         valorBoleta = 20000
         descuento = valorBoleta * descuentoXtreme_sillas   
         valorTotal = valorBoleta - descuento
-        
+           
         informacion_final["atraccion"]= atraccion
         informacion_final["apto"]= True
         informacion_final["total_boleta"] = valorTotal
-    
-    elif informacion_cliente.get("edad") > 18 and informacion_cliente.get("primer_ingreso") == False:
+
+    elif informacion.get("edad") > 18 and informacion.get("primer_ingreso") == False:
         atraccion = "X-Treme"
         valorBoleta = 20000
         informacion_final["atraccion"]= atraccion
         informacion_final["apto"]= True
         informacion_final["total_boleta"] = valorBoleta
-        
+
     # >= 15 y <= 18 :
-    elif informacion_cliente.get("edad") >= 15 and informacion_cliente.get("primer_ingreso") == True:
+    elif informacion.get("edad") >= 15 and informacion.get("primer_ingreso") == True:
         atraccion = "Carros chocones"
         valorBoleta = 5000
         descuento = valorBoleta * descuentoCarrochocones   
         valorTotal = valorBoleta - descuento
-        
+
         informacion_final["atraccion"]  = atraccion
         informacion_final["apto"]=True
         informacion_final["total_boleta"]=valorTotal
-        
-    elif  informacion_cliente.get("edad") >= 15 and informacion_cliente.get("primer_ingreso") == False:
+
+    elif  informacion.get("edad") >= 15 and informacion.get("primer_ingreso") == False:
         atraccion = "Carros chocones"
         valorBoleta = 5000
         informacion_final["atraccion"]  = atraccion
         informacion_final["apto"]=True
         informacion_final["total_boleta"]=valorBoleta 
-    
-    elif informacion_cliente.get("edad") >= 7  and informacion_cliente.get("primer_ingreso") == True:
+
+    elif informacion.get("edad") >= 7  and informacion.get("primer_ingreso") == True:
         atraccion = "Sillas voladoras"
         valorBoleta = 10000
         descuento = valorBoleta * descuentoXtreme_sillas  
         valorTotal = valorBoleta - descuento
-        
+
         informacion_final["atraccion"]  = atraccion
         informacion_final["apto"]=True
         informacion_final["total_boleta"]=valorTotal 
-        
-    elif informacion_cliente.get("edad") >= 7  and informacion_cliente.get("primer_ingreso") == False:
+
+    elif informacion.get("edad") >= 7  and informacion.get("primer_ingreso") == False:
         atraccion = "Sillas voladoras"
         valorBoleta = 10000
         descuento = valorBoleta * descuentoXtreme_sillas  
         valorTotal = valorBoleta - descuento
-        
+
         informacion_final["atraccion"]  = atraccion
         informacion_final["apto"]=True
         informacion_final["total_boleta"]=valorBoleta 
-        
-        
+
+
     else:
         informacion_final["apto"] = False
         informacion_final["atraccion"]  = "N/A"
         informacion_final["total_boleta"] ="N/A"
-        
-        
+    
     return informacion_final
-    
-    
-                
-print(cliente(4 ,"Tatiana Ruiz" ,18,False ))
 
+print(cliente(informacion))
