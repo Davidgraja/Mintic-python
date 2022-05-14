5ea5aab5593aba8fd0af2d67edd23f65f4fa9491
Davidgraja <juliograjalesparedes127@gmail.com>
2022-05-14 20:33:24 +0200

    sulucione el problema de return ,  ademas se termino el reto 2, hay que probarlo :)

--- reto2.py
+++ reto2.py
@@ -1,26 +1,78 @@
 def cliente(id_cliente,nombre,edad,primer_ingreso):
-    informacion_cliente = dict(nombre=nombre, edad=edad, atraccion="", apto="",primer_ingreso = primer_ingreso, total_boleta =""  )
-    
+    informacion_cliente= dict(id_cliente= id_cliente, nombre= nombre,edad=edad,primer_ingreso=primer_ingreso)
+      
     descuentoXtreme_sillas = 0.05
-    descuentoCarrochocones = 0.07
-    # informacionfinal = []
+    descuentoCarrochocones = 0.07    
+    informacion_final = dict(nombre = informacion_cliente["nombre"],edad=informacion_cliente["edad"],atraccion="",apto= "", primer_ingreso=informacion_cliente["primer_ingreso"], total_boleta="" )
+    
     """validacion"""
 
-
+    #> de 18 años:
     if informacion_cliente.get("edad") > 18 and informacion_cliente.get("primer_ingreso") ==True : 
         atraccion = "X-Treme"
         valorBoleta = 20000
         descuento = valorBoleta * descuentoXtreme_sillas   
         valorTotal = valorBoleta - descuento
         
-        informacion_cliente["atraccion"]= atraccion
-        informacion_cliente["apto"]= True
-        informacion_cliente["total_boleta"] = valorTotal
-        # informacionfinal = informacion_cliente
-    # return informacionfinal    
+        informacion_final["atraccion"]= atraccion
+        informacion_final["apto"]= True
+        informacion_final["total_boleta"] = valorTotal
     
+    elif informacion_cliente.get("edad") > 18 and informacion_cliente.get("primer_ingreso") == False:
+        atraccion = "X-Treme"
+        valorBoleta = 20000
+        informacion_final["atraccion"]= atraccion
+        informacion_final["apto"]= True
+        informacion_final["total_boleta"] = valorBoleta
+        
+    # >= 15 y <= 18 :
+    elif informacion_cliente.get("edad") >= 15 and informacion_cliente.get("primer_ingreso") == True:
+        atraccion = "Carros chocones"
+        valorBoleta = 5000
+        descuento = valorBoleta * descuentoCarrochocones   
+        valorTotal = valorBoleta - descuento
+        
+        informacion_final["atraccion"]  = atraccion
+        informacion_final["apto"]=True
+        informacion_final["total_boleta"]=valorTotal
+        
+    elif  informacion_cliente.get("edad") >= 15 and informacion_cliente.get("primer_ingreso") == False:
+        atraccion = "Carros chocones"
+        valorBoleta = 5000
+        informacion_final["atraccion"]  = atraccion
+        informacion_final["apto"]=True
+        informacion_final["total_boleta"]=valorBoleta 
     
+    elif informacion_cliente.get("edad") >= 7  and informacion_cliente.get("primer_ingreso") == True:
+        atraccion = "Sillas voladoras"
+        valorBoleta = 10000
+        descuento = valorBoleta * descuentoXtreme_sillas  
+        valorTotal = valorBoleta - descuento
+        
+        informacion_final["atraccion"]  = atraccion
+        informacion_final["apto"]=True
+        informacion_final["total_boleta"]=valorTotal 
+        
+    elif informacion_cliente.get("edad") >= 7  and informacion_cliente.get("primer_ingreso") == False:
+        atraccion = "Sillas voladoras"
+        valorBoleta = 10000
+        descuento = valorBoleta * descuentoXtreme_sillas  
+        valorTotal = valorBoleta - descuento
+        
+        informacion_final["atraccion"]  = atraccion
+        informacion_final["apto"]=True
+        informacion_final["total_boleta"]=valorBoleta 
+        
+        
+    else:
+        informacion_final["apto"] = False
+        informacion_final["atraccion"]  = "N/A"
+        informacion_final["total_boleta"] ="N/A"
+        
+        
+    return informacion_final
     
     
                 
-cliente(1 ,"Johana Fernandez" , 20 , True)
+print(cliente(4 ,"Tatiana Ruiz" ,18,False ))
+
