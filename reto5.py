f89b0bf800455eb0df2ab5681f020e181e15d239
Davidgraja <juliograjalesparedes127@gmail.com>
2022-06-09 22:58:43 +0200

    reto 5 terminado

--- reto5.py
+++ reto5.py
@@ -1,5 +1,43 @@
 import pandas as pd
 
-dicccionario = {"CO":100,"AR":250,"MX":150}
-new = pd.Series(dicccionario)
-print(new)
+rutaFileCsv = "https://github.com/luisguillermomolero/MisionTIC2022_2/blob/master/Modulo1_Python_MisionTIC2022_Main/Semana_5/Reto/movies.csv?raw=true"
+
+
+def listaPeliculas(rutaFileCsv:str):
+    import pathlib
+    
+    extencion = pathlib.Path(rutaFileCsv)
+    if extencion.suffix ==".csv?raw=true":
+        try:
+            leer_csv = pd.read_csv(rutaFileCsv)
+            dt = pd.DataFrame(leer_csv.pivot_table(index=["Country","Language"],values="Gross Earnings"))
+            return dt.head(10)
+        except Exception :
+            print("Error al leer el archivo de datos")
+        
+    else:
+        print("Extensión inválida.")
+
+
+print(listaPeliculas(rutaFileCsv))
+
+
+
+# def listaPeliculas(rutaFileCsv:str) -> str : 
+
+    
+#     if rutaFileCsv.split(".")[-1] =="csv?raw=true":
+#         try:
+#             leer_csv = pd.read_csv(rutaFileCsv)
+#             dt = leer_csv[["Country","Language","Gross Earnings"]]
+#             ganacias =dt.pivot_table(index=["Country","Language"])
+#             return ganacias.head(10)
+#         except:
+#             print("Error al leer el archivo de datos.")
+        
+#     else:
+#         print("Extensión inválida.")
+
+#     return
+
+
